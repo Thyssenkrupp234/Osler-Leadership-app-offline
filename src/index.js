a822430b93ebe3f16259e0ff2de77b464e2c0f20
Thyssenkrupp234 <thyssenkrupp234@gmail.com>
2021-12-11 14:54:32 -0800

    stop devtools from auto-opening

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  mainWindow.webContents.closeDevTools();
 };
 
 // This method will be called when Electron has finished
